d2a845c3643c375861a13cf96f0b8049aa8ba51a
Jose Llanos <chepe159@gmail.com>
2020-08-23 11:12:08 -0500

    Update cachematrix.R
    Jose Miguel Llanos Mosquera

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,6 @@
 # function makeCacheMatrix: creates a special matrix object that can cache its inverse
 makeCacheMatrix <- function(m = matrix()){   
+	    
 	    # initialize inverse    
         i <- NULL
         	
@@ -29,6 +30,7 @@ makeCacheMatrix <- function(m = matrix()){
 
 # function cacheSolve: computes the inverse of the special "matrix" returned
 cacheSolve <- function(x, ...) {
+	    
 	    # return matrix inverse
         m <- x$get_inverse()	
         
